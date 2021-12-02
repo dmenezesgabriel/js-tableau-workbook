e612abb8b767718fcbf91fa139253689753214dd
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2021-12-02 09:25:33 -0300

    Create main function

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-var reader = new FileReader();
-let parser = new DOMParser();
+function parseText(xmlDoc) {
+  let workbookElements = { datasources: [] };
 
-function parseText(xml) {
-  let xmlDoc = parser.parseFromString(xml, "text/xml");
   let datasources = xmlDoc.getElementsByTagName("datasources")[0].children;
-  console.log(datasources);
   //  Datasources
   for (let datasource of datasources) {
+    datasourceObj = { name: "", connections: [] };
     // Data Source
     let datasourceCaption = datasource.getAttribute("caption");
-    console.log(datasource);
+    datasourceObj.name = datasourceCaption;
     // Connections
     let connections = datasource.getElementsByTagName("connection");
     for (let index = 0; index < connections.length; index++) {
       // Connection
       let connection = connections[index];
-      console.log(connection);
-      let connectionClass = connection.getAttribute("class");
-      console.log(connectionClass);
-      connectionClass = connection.getAttribute("class");
+      datasourceObj.connections.push(connection);
+    }
+    workbookElements.datasources.push(datasourceObj);
+  }
+  return workbookElements;
+}
+
+function changeWorkbookElement(workbookElements) {
+  for (let datasource of workbookElements.datasources) {
+    for (let connection of datasource.connections) {
+      console.log(connection.getAttribute("class"));
       connection.setAttribute("class", "batata");
-      connectionClass = connection.getAttribute("class");
-      console.log(connectionClass);
+      console.log(connection.getAttribute("class"));
     }
   }
 }
 
-fetch("/samples/sample.twb")
-  .then((r) => {
-    return r.text();
-  })
-  .then((text) => {
-    parseText(text);
-  });
+function createForm(workbookElements) {
+  let outputDiv = document.querySelector("#output");
+
+  for (let datasource of workbookElements.datasources) {
+    for (let connection of datasource.connections) {
+    }
+  }
+}
+
+async function getFile(url) {
+  let content;
+  await fetch(url)
+    .then((response) => {
+      return response.text();
+    })
+    .then((text) => {
+      content = text;
+    });
+  return content;
+}
+
+async function main() {
+  const parser = new DOMParser();
+
+  content = await getFile("/samples/sample.twb");
+  let xmlDoc = parser.parseFromString(content, "text/xml");
+  workbookElements = parseText(xmlDoc);
+  // changeWorkbookElement(workbookElements);
+}
+
+main();
