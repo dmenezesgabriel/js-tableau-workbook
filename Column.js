dcc27cc0adc2bda6cc546bdbcc1d250f40cfba0c
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2021-12-05 20:43:33 -0300

    Add fields class

---
+++ Column.js
@@ -0,0 +1,77 @@
+export default class Column {
+  constructor(columnXML) {
+    this._columnXML = columnXML;
+    this._name = columnXML.getAttribute("name");
+    this._caption = columnXML.getAttribute("caption");
+    this._datatype = columnXML.getAttribute("datatype");
+    this._role = columnXML.getAttribute("role");
+    this._type = columnXML.getAttribute("type");
+    this._calculation = this._prepareCalculation(this._columnXML);
+    this._description = columnXML.getAttribute("description");
+    this._usedIn = [];
+  }
+
+  _prepareCalculation(columnXML) {
+    let formula = null;
+    let calculationElement = columnXML.getElementsByTagName("calculation")[0];
+    if (calculationElement) {
+      formula = calculationElement.getAttribute("formula");
+    }
+    return formula;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    return (this._name = name);
+  }
+
+  get caption() {
+    return this._caption;
+  }
+
+  set caption(caption) {
+    return (this._caption = caption);
+  }
+  get datatype() {
+    return this._datatype;
+  }
+
+  set datatype(datatype) {
+    return (this._datatype = datatype);
+  }
+
+  get role() {
+    return this._role;
+  }
+
+  set role(role) {
+    return (this._role = role);
+  }
+
+  get calculation() {
+    return this._calculation;
+  }
+
+  set calculation(calculation) {
+    return (this._calculation = calculation);
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  set description(description) {
+    return (this._description = description);
+  }
+
+  get usedIn() {
+    return this._usedIn;
+  }
+
+  set usedIn(usedIn) {
+    return (this._usedIn = usedIn);
+  }
+}
--- Datasource.js
+++ Datasource.js
@@ -1,4 +1,5 @@
 import Connection from "./Connection.js";
+import Column from "./Column.js";
 
 export default class Datasource {
   constructor(datasourceXML, fileName = null) {
@@ -8,6 +9,7 @@ export default class Datasource {
     this._version = this._datasourceXML.getAttribute("version");
     this._caption = this._datasourceXML.getAttribute("caption");
     this._connections = this._prepareConnections(this._datasourceXML);
+    this._columns = this._prepareColumns(this._datasourceXML);
   }
 
   _prepareConnections(datasourceXML) {
@@ -19,6 +21,17 @@ export default class Datasource {
       connections.push(connection);
     }
     return connections;
+  }
+
+  _prepareColumns(datasourceXML) {
+    let columns = [];
+    let columnsElements = datasourceXML.getElementsByTagName("column");
+    for (let index = 0; index < columnsElements.length; index++) {
+      // column
+      let column = new Column(columnsElements[index]);
+      columns.push(column);
+    }
+    return columns;
   }
 
   get name() {
